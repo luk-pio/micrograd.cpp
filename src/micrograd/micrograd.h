4069f4c226a1c8b5837e062329048901fb1c3b5f
luk-pio <luke.piotrak@github.com>
2025-01-15 07:42:07 +0000

    feat: add plus operator

--- src/micrograd/micrograd.h
+++ src/micrograd/micrograd.h
@@ -3,10 +3,10 @@
 
 class Value {
 public:
-    double data;
-
-    explicit Value(double d);
-
+    // Working under the assumption that float has enough precision and we don't care about overflow
+    float data;
+    explicit Value(float d);
     friend std::ostream& operator<<(std::ostream& os, const Value& value);
+    friend Value operator+(const Value& value1, const Value& value2);
 };
 
