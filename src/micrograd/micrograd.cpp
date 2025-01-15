017c234f91434cba39ff7d84702301c179df1976
luk-pio <luke.piotrak@github.com>
2025-01-15 07:58:41 +0000

    feat: add minus operator

--- src/micrograd/micrograd.cpp
+++ src/micrograd/micrograd.cpp
@@ -11,3 +11,11 @@ std::ostream& operator<<(std::ostream& os, const Value& value) {
 Value operator+(const Value& value1, const Value& value2) {
   return Value(value1.data + value2.data);
 }
+
+Value operator-(const Value& value1, const Value& value2) {
+  return Value(value1.data - value2.data);
+}
+
+Value operator-(const Value& value1) {
+  return Value(-value1.data);
+}
