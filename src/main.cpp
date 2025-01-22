ed1555109de03f23086d950538a5bdcec1b8faad
luk-pio <luke.piotrak@github.com>
2025-01-22 08:03:09 +0000

    feat: add relu operation

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 #include <micrograd.h>
 
 int main() {
-	Value value(1);
-	std::cout << value << std::endl;
-	return 0;
+  Value inputs = Value(1) * Value(3) + Value(2) * Value(4) +
+                 Value(10) * Value(8) + Value(0.1) * Value(0.3);
+
+  Value result = inputs.ReLU();
+  result.print_graph(std::cout) << std::endl;
+  return 0;
 }
